fa14fdf15d9232a66b8ff3aff6f2ee906af9ed6e
Richard Simões <rsimoes@cpan.org>
2013-03-04 18:50:46 +0000

    Import of RSIMOES/Regexp-Common-VATIN-v0.0.7 from CPAN.
    gitpan-cpan-distribution: Regexp-Common-VATIN
    gitpan-cpan-version:      v0.0.7
    gitpan-cpan-path:         RSIMOES/Regexp-Common-VATIN-v0.0.7.tar.gz
    gitpan-cpan-author:       RSIMOES
    gitpan-cpan-maturity:     released

--- lib/Regexp/Common/VATIN.pm
+++ lib/Regexp/Common/VATIN.pm
@@ -5,7 +5,7 @@ use warnings FATAL => "all";
 use utf8;
 use Regexp::Common qw(pattern clean no_defaults);
 
-our $VERSION = 'v0.0.6'; # VERSION
+our $VERSION = 'v0.0.7'; # VERSION
 # ABSTRACT: Patterns for matching EU VAT Identification Numbers
 
 my $a  = "[a-zA-Z]";
@@ -14,9 +14,9 @@ my $d  = "[0-9]";
 my $s  = "[ ]?";
 
 # repeats:
-my ($r2, $r3, $r4, $r5, $r8, $r9, $r10, $r11, $r12) = map {
+my ($r2, $r3, $r4, $r5, $r7, $r8, $r9, $r10, $r11, $r12) = map {
     "{" . $_ . "}"
-} 2..5, 8..12;
+} 2..5, 7..12;
 
 my %patterns = (
     AT => "U$d$r8",                   # Austria
@@ -28,10 +28,10 @@ my %patterns = (
     DK => "(?:$d$r2$s){3}$d$r2",      # Denmark
     EE => "$d$r9",                    # Estonia
     EL => "$d$r9",                    # Greece
-    ES => "$d$r9",                    # Spain
+    ES => "$an$d$r7$an",              # Spain
     FI => "$d$r8",                    # Finland
     FR => "$an$r2$s$d$r9",            # France
-    GB => do {                                # United Kingdom
+    GB => do {                        # United Kingdom
         my $multi_block  = "$d$r3$s$d$r4$s$d$r2$s(?:$d$r3)?";
         my $single_block = "(?:GD|HA)$d$r3";
         "(?:$multi_block|$single_block)";
@@ -43,7 +43,7 @@ my %patterns = (
     LU => "$d$r8",                    # Luxembourg
     LV => "$d$r11",                   # Latvia
     MT => "$d$r8",                    # Malta
-    NL => "$d$r12",                   # The Netherlands
+    NL => "$d${r9}[bB]$d$r2",         # The Netherlands
     PL => "$d$r10",                   # Poland
     PT => "$d$r9",                    # Portugal
     RO => "${d}{2,10}",               # Romania
